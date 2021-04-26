c518b7b0a011a51e9d25f46efdb19247e8dcf077
yekta <yekta.yuksel@gmail.com>
2021-04-26 15:22:25 +0300

    Database bağlantısı yapıldı

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
 
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.cj.xdevapi.Statement;
+
+
+
 public class Main 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws ClassNotFoundException, SQLException 
 	{
 		
 		//new Screen(new OpeningPanel());
 		//new Screen(new AdminPanel());
-		new Screen(new FreeUserPanel());
-	}
+		//new Screen(new FreeUserPanel());
+		SpotifyDB db = new SpotifyDB();
+		//db.createTable();
+		db.viewTable();
 
+		
+		 
+    }
+	
 }
+
+
---
+++ src/SpotifyDB.java
@@ -0,0 +1,68 @@
+import java.sql.*;
+
+
+import com.mysql.cj.xdevapi.Statement;
+
+public class SpotifyDB 
+{
+	
+	static Connection conn;
+	SpotifyDB() throws ClassNotFoundException
+	{
+		try
+	    {
+	      // create our mysql database connection
+	      //String myDriver = "com.mysql.cj.jdbc.Driver";
+	      String myUrl = "jdbc:mysql://localhost:3306/sql_spotify";
+	      //Class.forName(myDriver);
+	      conn = DriverManager.getConnection(myUrl, "root", "malcanus12");
+	      
+	      // our SQL SELECT query. 
+	      // if you only need a few columns, specify them by name instead of using "*"
+	     
+	    }
+	    catch (Exception e)
+	    {
+	      System.err.println("Got an exception! ");
+	      System.err.println(e.getMessage());
+	    }
+	}
+	
+	public void createTable() throws SQLException, ClassNotFoundException
+	{
+		
+	}
+	public void viewTable() throws SQLException {
+		 String query = "SELECT * FROM sarki";
+
+	      // create the java statement
+	      java.sql.Statement st = conn.createStatement();
+	      
+	      // execute the query, and get a java resultset
+	      ResultSet rs = ((java.sql.Statement) st).executeQuery(query);
+	      
+	      // iterate through the java resultset
+	      while (rs.next())
+	      {
+	        int SarkiID = rs.getInt("SarkiID");
+	        int SanatciID = rs.getInt("SarkiID");
+	        int AlbumID = rs.getInt("SarkiID");
+	        String tarih = rs.getString("tarih");
+	        String Tur = rs.getString("Tur");
+	        int sure = rs.getInt("sure");
+	        int dinlenmeSayisi = rs.getInt("dinlenmeSayisi");
+	        // print the results
+	        System.out.format("%d, %d, %d, %s, %s,  %d, %d \n", SarkiID, SanatciID, AlbumID,  tarih, Tur, sure, dinlenmeSayisi);
+	      }
+	    st.close();
+	  }
+	
+	public ArrayList<String> getAlbumNames(String SanatciAdi)
+	{
+		String query = "SELECT AlbumAdi FROM Sanatci S, Album A Where S.SanatciID = A.SanatciID and S.SanatciAdi = " + SanatciAdi;
+	}
+	   
+	  
+	
+
+}
